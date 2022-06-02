44a7ab30d9f139ef4077f98d59e1ce57bd66d4d2
anacarolinavoto <anacarolina.voto@gmail.com>
2022-06-02 17:27:09 -0300

    Array
    dados

---
+++ includes/_dados.php
@@ -0,0 +1,17 @@
+<?php
+
+$produtos = array(
+   0 => array('nome' => 'motorista', 'preço' => '5.000', 'descricao' => 'macaquinho com chapéu preto', 'imagem' => 'nft0.jpg'),
+   1 => array('nome' => 'rei', 'preco' => '3.000', 'descricao' => 'macaquinho rei colorido', 'imagem' => 'nft1.jpg'),
+   2 => array('nome' => 'jorge curioso', 'preco' => '4.500', 'descricao' => 'macaquinho pescador', 'imagem' => 'nft2.jpg'),
+   3 => array('nome' => 'azuleto', 'preco' => '1.500', 'descricao' => 'macaquinho chato azul', 'imagem' => 'nft3.jpg'),
+   4 => array('nome' => 'capitão', 'preco' => '12.000', 'descricao' => 'macaquinho capitão lazer nos olhos', 'imagem' => 'nft4.jpg'),
+   5 => array('nome' => 'americano', 'preco' => '3.000', 'descricao' => 'macaquinho radical motoqueiro', 'imagem' => 'nft5.jpg'),
+   6 => array('nome' => 'robô', 'preco' => '5.500', 'descricao' => 'macaquinho sem olho robótico', 'imagem' => 'nft6.jpg'),
+   7 => array('nome' => 'pink', 'preco' => '11.000', 'descricao' => 'macaquinho rosa de chapéu', 'imagem' => 'nft7.jpg'),
+   8 => array('nome' => 'cowboy', 'preco' => '40.000', 'descricao' => 'macaquinho de rodeio', 'imagem' => 'nft8.jpg'),
+   9 => array('nome' => 'punk', 'preco' => '3.000', 'descricao' => 'macaquinho encrenqueiro', 'imagem' => 'nft9.jpg'),
+  10 => array('nome' => 'piloto', 'preco' => '6.900', 'descricao' => 'macaquinho triste de sutiã', 'imagem' => 'nft10.jpg'),
+);
+
+?>
